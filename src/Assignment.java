4155e469d63e957a5f1c8a91034d2e9b0f38b11d
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-07 13:37:28 -0400

    Initial commit

---
+++ src/Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        int myPounds = 200;
+        double toKilograms = 0.45359237;
+        System.out.println(myPounds * toKilograms);
+
+
+    }
+
+}
